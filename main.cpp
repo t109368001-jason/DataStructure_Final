1a651f51c4a06474ea43d4b715005757db2e57ec
賢傑 蕭 <wesdxc0607@gmail.com>
2018-05-14 18:22:02 +0800

    20180514_1821

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
+#include <iostream>
 #include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/surface/gp3.h>
+#include <pcl/surface/marching_cubes_rbf.h>
 #include <math.h>
 #include <vector>
 #include <gl/GL.h>
 #include <gl/GLU.h>
 #include <gl/glut.h>
+#include <wingdi.h>
 
 #define CAMERA_MOVE_SPEED			0.05f			//
 #define CAMERA_ROTATE_SPEED			20.0f			//20 deg per 100 pixel
-#define VIEWER_WIDTH				1024.0f
-#define VIEWER_HEIGHT				768.0f
+#define VIEWER_WIDTH				1920.0f
+#define VIEWER_HEIGHT				1080.0f
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 pcl::PolygonMesh triangle;
@@ -33,6 +36,8 @@ GLfloat xClick = -1, yClick = -1;
 BOOL fixPhi = false;
 BOOL fixTheta = false;
 BOOL meshFill = false;
+uint32_t filename = 1;
+
 
 class Viewer
 {
@@ -47,6 +52,7 @@ public:
 	{
 		for (size_t i = 0; i < cloud->points.size(); ++i)
 		{
+			
 			glBegin(GL_POINTS);
 			glVertex3f(cloud->points[i].x, cloud->points[i].y , cloud->points[i].z );
 			glEnd();
@@ -54,7 +60,6 @@ public:
 	}
 	void draw(pcl::PolygonMesh &mesh)		//Draw mesh
 	{
-
 		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 		pcl::fromPCLPointCloud2(mesh.cloud, *cloud);
 
@@ -63,13 +68,13 @@ public:
 			if (meshFill)
 			{
 				//有BUG的光影
-				std::cout << triangle.cloud.data[i + 0] << "\t" << triangle.cloud.data[i + 0] << "\t" << triangle.cloud.data[i + 0] << std::endl;
-				GLfloat line1X = cloud->points[mesh.polygons[i].vertices[0]].x - cloud->points[mesh.polygons[i].vertices[1]].x;;
-				GLfloat line1Y = cloud->points[mesh.polygons[i].vertices[0]].y - cloud->points[mesh.polygons[i].vertices[1]].y;;
-				GLfloat line1Z = cloud->points[mesh.polygons[i].vertices[0]].z - cloud->points[mesh.polygons[i].vertices[1]].z;;
-				GLfloat line2X = cloud->points[mesh.polygons[i].vertices[1]].x - cloud->points[mesh.polygons[i].vertices[2]].x;;
-				GLfloat line2Y = cloud->points[mesh.polygons[i].vertices[1]].y - cloud->points[mesh.polygons[i].vertices[2]].y;;
-				GLfloat line2Z = cloud->points[mesh.polygons[i].vertices[1]].z - cloud->points[mesh.polygons[i].vertices[2]].z;;
+				//std::cout << triangle.cloud.data[i + 0] << "\t" << triangle.cloud.data[i + 0] << "\t" << triangle.cloud.data[i + 0] << std::endl;
+				GLfloat line1X = cloud->points[mesh.polygons[i].vertices[0]].x - cloud->points[mesh.polygons[i].vertices[1]].x;
+				GLfloat line1Y = cloud->points[mesh.polygons[i].vertices[0]].y - cloud->points[mesh.polygons[i].vertices[1]].y;
+				GLfloat line1Z = cloud->points[mesh.polygons[i].vertices[0]].z - cloud->points[mesh.polygons[i].vertices[1]].z;
+				GLfloat line2X = cloud->points[mesh.polygons[i].vertices[1]].x - cloud->points[mesh.polygons[i].vertices[2]].x;
+				GLfloat line2Y = cloud->points[mesh.polygons[i].vertices[1]].y - cloud->points[mesh.polygons[i].vertices[2]].y;
+				GLfloat line2Z = cloud->points[mesh.polygons[i].vertices[1]].z - cloud->points[mesh.polygons[i].vertices[2]].z;
 				GLfloat nX = line1Y * line2Z - line2Y * line1Z;
 				GLfloat nY = line1Z * line2X - line2Z * line1X;
 				GLfloat nZ = line1X * line2Y - line2X * line1Y;
@@ -129,6 +134,54 @@ public:
 	}
 };
 Viewer viewer;
+
+void mouseMove(int x, int y);
+void SaveAsBMP(const char *fileName)
+{
+	FILE *file;
+	unsigned long imageSize;
+	GLbyte *data = NULL;
+	GLint viewPort[4];
+	GLenum lastBuffer;
+	BITMAPFILEHEADER bmfh;
+	BITMAPINFOHEADER bmih;
+	bmfh.bfType = 'MB';
+	bmfh.bfReserved1 = 0;
+	bmfh.bfReserved2 = 0;
+	bmfh.bfOffBits = 54;
+	glGetIntegerv(GL_VIEWPORT, viewPort);
+	imageSize = ((viewPort[2] + ((4 - (viewPort[2] % 4)) % 4))*viewPort[3] * 3) + 2;
+	bmfh.bfSize = imageSize + sizeof(bmfh) + sizeof(bmih);
+	data = (GLbyte*)malloc(imageSize);
+	glPixelStorei(GL_PACK_ALIGNMENT, 4);
+	glPixelStorei(GL_PACK_ROW_LENGTH, 0);
+	glPixelStorei(GL_PACK_SKIP_ROWS, 0);
+	glPixelStorei(GL_PACK_SKIP_PIXELS, 0);
+	glPixelStorei(GL_PACK_SWAP_BYTES, 1);
+	glGetIntegerv(GL_READ_BUFFER, (GLint*)&lastBuffer);
+	glReadBuffer(GL_FRONT);
+	glReadPixels(0, 0, viewPort[2], viewPort[3], GL_RGB, GL_UNSIGNED_BYTE, data);
+	data[imageSize - 1] = 0;
+	data[imageSize - 2] = 0;
+	glReadBuffer(lastBuffer);
+	file = fopen(fileName, "wb");
+	bmih.biSize = 40;
+	bmih.biWidth = viewPort[2];
+	bmih.biHeight = viewPort[3];
+	bmih.biPlanes = 1;
+	bmih.biBitCount = 24;
+	bmih.biCompression = 0;
+	bmih.biSizeImage = imageSize;
+	bmih.biXPelsPerMeter = 45089;
+	bmih.biYPelsPerMeter = 45089;
+	bmih.biClrUsed = 0;
+	bmih.biClrImportant = 0;
+	fwrite(&bmfh, sizeof(bmfh), 1, file);
+	fwrite(&bmih, sizeof(bmih), 1, file);
+	fwrite(data, imageSize, 1, file);
+	free(data);
+	fclose(file);
+}
 void Display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -138,7 +191,8 @@ void Display(void)
 	glLineWidth(1.0);
 
 	glLoadIdentity();
-
+	xClick = 0;
+	mouseMove(5, 0);
 	gluLookAt(cameraX, cameraY, cameraZ, cameraX + lookX, cameraY + lookY, cameraZ + lookZ, 0.0f, 1.0f, 0.0f);
 
 	viewer.draw(triangle);
@@ -148,7 +202,18 @@ void Display(void)
 	viewer.draw(10, 32, "Mouse drag : Rotate object");
 	viewer.draw(10, 16, "Mouse scroll : Zoom");
 
+
 	glutSwapBuffers();
+
+	if (filename < 1000)
+	{
+		std::stringstream ss;
+		ss << "../picture/";
+		ss << filename++;
+		ss << ".bmp";
+		SaveAsBMP(&(*ss.str().begin()));
+		glutPostRedisplay();
+	}
 }
 
 void Mouse(int button, int state, int x, int y)
